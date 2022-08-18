483fb0a4cd207b998e9640e4953e4e803a3851cd
johnbane85 <junyu.siew@gmail.com>
2022-08-18 20:41:51 +0800

    8th commit

--- src/main/java/myapp/core/day18_cart/controllers/CartController.java
+++ src/main/java/myapp/core/day18_cart/controllers/CartController.java
@@ -1,8 +1,14 @@
 package myapp.core.day18_cart.controllers;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,21 +17,49 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(path = "/cart")
 public class CartController {
 
-  @PostMapping
-  public String postCart(@RequestBody MultiValueMap<String, String> form, Model model) {
+  @GetMapping
+  public String getCart(Model model, HttpSession sess) {
 
-    String name = form.getFirst("name");
-    if ((null == name) || (name.trim().length() <= 0))
-      name = "anonymous";
+    String name = (String) sess.getAttribute("name");
+    List<String> cart = (List<String>) sess.getAttribute("cart");
 
-    String item = form.getFirst("item");
-    String quantity = form.getFirst("quantity");
+    if (isNull(name))
+      return "redirect:index.html";
 
     model.addAttribute("name", name.toUpperCase());
-    model.addAttribute("item", item);
-    model.addAttribute("quantity", quantity);
+    model.addAttribute("cart", cart);
 
     return "cart";
+  }
 
+  @PostMapping
+  public String postCart(@RequestBody MultiValueMap<String, String> form, Model model, HttpSession sess) {
+
+    List<String> cart = null;
+
+    String name = form.getFirst("name");
+    if (!isNull(name)) {
+      // new session
+      System.out.println("name not in session");
+      sess.setAttribute("name", name);
+      cart = new LinkedList<>();
+      sess.setAttribute("cart", cart);
+
+    }
+
+    name = (String) sess.getAttribute("name");
+    cart = (List<String>) sess.getAttribute("cart");
+    String item = form.getFirst("item");
+    if (!isNull(item))
+      cart.add(item);
+
+    model.addAttribute("name", name.toUpperCase());
+    model.addAttribute("cart", cart);
+
+    return "cart";
+  }
+
+  private boolean isNull(String s) {
+    return ((null == s) || (s.trim().length() <= 0));
   }
 }
